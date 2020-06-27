29949aed1cbc2f34c6edd78d305bb5b3776e3e8a
John Albert <34950240+Johncs2d@users.noreply.github.com>
2020-06-27 11:16:49 +0800

    Update connection.php

--- web/backend/connection.php
+++ web/backend/connection.php
@@ -2,10 +2,10 @@
 function connect(){
 try {
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-  $conn = new PDO("mysql:host=$servername;dbname=curay_laundry", $username, $password);
+$servername = "ec2-52-72-221-20.compute-1.amazonaws.com";
+$username = "rnvaghuuaczinn";
+$password = "4988d30e4605549a95ee850a9b6e476ad15ffcd45f455caca603c09af05f53a5";
+  $conn = new PDO("mysql:host=$servername;dbname=ddtlhpaskt5v62", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
